5ea8f3a2c7f8f4ac3a0ecf317fbadc554477fea6
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2020-12-29 21:17:19 +0200

    API retreive test

--- SRC/API-DATA-RETRIEVE.py
+++ SRC/API-DATA-RETRIEVE.py
@@ -17,6 +17,13 @@ def push_csv(curser):
         query = 'insert into movie_names (id, name) values (%d, %s)'
         curser.execute(query, query_params) 
 
+# retrieve data from API
+
+result = requests.get('https://api.themoviedb.org/3/movie/550?api_key=7e759b2920f15726a47aecff3b17d4fb')
+# check result
+result_dict = json.loads(result)
+print(result_dict['id'])
+
 # connection to server details
 
 # my_sql_connector
@@ -33,13 +40,6 @@ cursor.close()
 ctx.close()
 
 # definitions on how to insert different data to DB
-
-# retrieve data from API
-
-result = requests.get('https://api.themoviedb.org/3/movie/550?api_key=7e759b2920f15726a47aecff3b17d4fb')
-# check result
-result_dict = json.loads(result)
-print(result_dict['id'])
 
 # my_path = os.path.abspath(os.path.dirname(__file__))
 # path = os.path.join(my_path, "static/data/movies.csv")
