e09ed4305f9fa3aa3062dbfa43816e751f4ea3ad
kozmi <kozminykhann@gmail.com>
2021-01-04 17:39:04 +0200

    db-create

--- SRC/API-DATA-RETRIEVE.py
+++ SRC/API-DATA-RETRIEVE.py
@@ -13,9 +13,14 @@ LOCAL = '127.0.0.1'
 helping methods
 '''
 
+
 # creating tables
 def create_tables(cursor):
-    query = 'CREATE TABLE IF NOT EXISTS movie_names (id INT PRIMARY KEY, name VARCHAR(100) NOT NULL)'
+    query = '''CREATE TABLE IF NOT EXISTS movie_names (id INT PRIMARY KEY, title VARCHAR(100) NOT NULL, genre VARCHAR(100),' \
+            ' duration INT, language VARCHAR(100), budget INT, income INT, year INT)'''
+    cursor.execute(query)
+
+    query = '''CREATE TABLE IF NOT EXISTS actors (id INT PRIMARY KEY, name VARCHAR(100) NOT NULL)'''
     cursor.execute(query)
 
 
@@ -28,13 +33,15 @@ def imdb_id_to_id(imdb_id):
 definitions on how to insert different data to DB
 '''
 
+
 # retrieve data form csv and insert
 def push_csv(curosr):
     df = pd.read_csv('./APPLICATION-SOURCE-CODE/static/data/movies.csv')
     for index, row in df.iterrows():
-        query_params = imdb_id_to_id(row['imdb_title_id']), row['title']
+        query_params = imdb_id_to_id(row['imdb_title_id']), row['title'], row['genre'], row['duration'], row['budget'],\
+                       row['worlwide_gross_income']
         # print('params: ', query_params)
-        query = 'insert into movie_names (id, name) values (%s, %s)'
+        query = '''insert into movie_names (id, title, genre, duration, budget, income) values (%s, %s, %s, %s, %s, %s)'''
         cursor.execute(query, query_params, multi=False) 
 
 
@@ -51,6 +58,8 @@ def push_movie(cursor, imdb_id, title):
 '''
 insert data to db
 '''
+
+
 def main(cursor):
     create_tables(cursor)
     push_csv(cursor)
