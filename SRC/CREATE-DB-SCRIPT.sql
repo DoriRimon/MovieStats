5acbd59bd09868746b7ff93218e4641c849cac04
kozmi <kozminykhann@gmail.com>
2021-01-06 10:49:21 +0200

    db_script

--- SRC/CREATE-DB-SCRIPT.sql
+++ SRC/CREATE-DB-SCRIPT.sql
@@ -1,6 +1,11 @@
 CREATE TABLE IF NOT EXISTS movie_names (
 	id INT PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
+    genre VARCHAR(100),
+    duration INT,
+    language VARCHAR(100),
+    budget INT,
+    year INT
 	)
 
 --- create fulltext index
