30485cd012d475de6281f364feeb8567eb332d8d
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2021-01-12 23:30:00 +0200

    minor changes

--- SRC/Database.py
+++ SRC/Database.py
@@ -51,7 +51,9 @@ class Database:
     def search_movie(self, text):
         query = ''' select  title
                     from    movie
-                    where   match(title) against({}* in boolean mode); '''.format(text)
+                    where   match(title) against('{}*' in boolean mode); '''.format(text)
+        
+        # query = ''' select  title  from    movie  where   match(title) against('+Fi* +O*' in boolean mode); '''
         movies = self.execute_query(query)
         return movies
 
