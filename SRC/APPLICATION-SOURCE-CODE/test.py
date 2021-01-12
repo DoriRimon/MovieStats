047d671b8c66988ff9b28cf0c3ee89e5a3e25c07
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2021-01-12 09:03:07 +0200

    minor changes

--- SRC/APPLICATION-SOURCE-CODE/test.py
+++ SRC/APPLICATION-SOURCE-CODE/test.py
@@ -1,6 +1,11 @@
 import os, json
 from flask import Flask, jsonify, request, redirect, render_template
-from globe import *
+
+NAME    = 'DbMysql04'
+HOST    = 'mysqlsrv1.cs.tau.ac.il'
+LOCAL   = '127.0.0.1'
+API_KEY = '7e759b2920f15726a47aecff3b17d4fb'
+PORT    = 44444
 
 app = Flask(__name__)
 
