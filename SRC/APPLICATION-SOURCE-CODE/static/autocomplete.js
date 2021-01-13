efabfe1fbe96f80b0fad7767d2aeabbb5784a6fb
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2021-01-13 05:30:07 +0200

    minor changes

--- SRC/APPLICATION-SOURCE-CODE/static/autocomplete.js
+++ SRC/APPLICATION-SOURCE-CODE/static/autocomplete.js
@@ -5,7 +5,7 @@ function autocomplete(inp) {
 
       var xhr = new XMLHttpRequest();
       xhr.open("POST", '/search', true);
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
 
       xhr.onreadystatechange = function() 
       {
