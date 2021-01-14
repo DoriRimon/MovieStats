0379f7c29951a5a2a7bf5a65eaf8bf6b23761090
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2021-01-14 20:18:51 +0200

    filter movie_actors

--- SRC/data_fill.py
+++ SRC/data_fill.py
@@ -10,15 +10,15 @@ from globe import *
 
 
 # extract csv files
-with zipfile.ZipFile('./APPLICATION-SOURCE-CODE/static/data/data.zip', 'r') as zip_ref:
-    zip_ref.extractall('./APPLICATION-SOURCE-CODE/static/data')
+with zipfile.ZipFile(PATH_PREFIX + '/APPLICATION-SOURCE-CODE/static/data/data.zip', 'r') as zip_ref:
+    zip_ref.extractall(PATH_PREFIX + '/APPLICATION-SOURCE-CODE/static/data')
 
 
 # return    : df
 # shape     : id, rating
 # content   : globe.MOVIES_BATCH_SIZE heighest rated movies
 def filter_movies_csv():
-    df = pd.read_csv("./APPLICATION-SOURCE-CODE/static/data/ratings.csv", usecols=['imdb_title_id', 'weighted_average_vote'])
+    df = pd.read_csv(PATH_PREFIX + "/APPLICATION-SOURCE-CODE/static/data/ratings.csv", usecols=['imdb_title_id', 'weighted_average_vote'])
     df.rename(columns={'imdb_title_id': 'id', 'weighted_average_vote': 'rating'}, inplace=True)
     df['id'] = df['id'].astype('str')
     exp = (df['id'].str.len() == 9)
@@ -38,14 +38,13 @@ def filter_actors_csv(movieActors_df):
 # shape     : movie_id, actor_id
 # content   : connects between movie ids and relevant (play in existing movies) actors ids
 def filter_movieActors_csv(movies_df):
-    df = pd.read_csv("./APPLICATION-SOURCE-CODE/static/data/movie_actors.csv", usecols=['imdb_title_id', 'imdb_name_id', 'category'])
+    df = pd.read_csv(PATH_PREFIX + "/APPLICATION-SOURCE-CODE/static/data/movie_actors.csv", usecols=['imdb_title_id', 'imdb_name_id', 'category'])
     df.rename(columns={'imdb_title_id': 'movie_id', 'imdb_name_id': 'actor_id'}, inplace=True)
     movies_df.rename(columns={'id' : 'movie_id'}, inplace=True)
     df = df.merge(movies_df, how='inner', on='movie_id')
     df['category'] = df['category'].astype('str')
     df = df.loc[(df['category'] == 'actress') | (df['category'] == 'actor')]
     df['actor_id'] = df['actor_id'].astype('str')
-    df = df.loc[df['actor_id'].str.len == 9]
+    df = df.loc[(df['actor_id'].str.len() == 9)]
     df = df.drop(['category', 'rating'], axis=1)
     return df
-    
--- SRC/globe.py
+++ SRC/globe.py
@@ -1,10 +1,11 @@
 # general global constants
 
-NAME    =               'DbMysql04'
-HOST    =               'mysqlsrv1.cs.tau.ac.il'
-LOCAL   =               '127.0.0.1'
-API_KEY =               '7e759b2920f15726a47aecff3b17d4fb'
-PORT    =               44444
-RUN_LOCALLY   =         False
-BASE_API_URL =          'https://api.themoviedb.org/3'
-MOVIES_BATCH_SIZE =     10_000
+NAME                =   'DbMysql04'
+HOST                =   'mysqlsrv1.cs.tau.ac.il'
+LOCAL               =   '127.0.0.1'
+API_KEY             =   '7e759b2920f15726a47aecff3b17d4fb'
+PORT                =   44444
+RUN_LOCALLY         =   False
+PATH_PREFIX         =   'SRC' if RUN_LOCALLY else '.'
+BASE_API_URL        =   'https://api.themoviedb.org/3'
+MOVIES_BATCH_SIZE   =   10_000
