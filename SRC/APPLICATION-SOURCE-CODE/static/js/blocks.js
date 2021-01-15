6c4ea09d04f3a250446cebbc1d4e04c42b775313
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2021-01-15 05:03:06 +0200

    added pictures to blocks page

--- SRC/APPLICATION-SOURCE-CODE/static/js/blocks.js
+++ SRC/APPLICATION-SOURCE-CODE/static/js/blocks.js
@@ -1,12 +1,28 @@
-function blocks(type, rec) {
+const basePosterPath = 'https://image.tmdb.org/t/p/' 
+const size = 'w780'
+
+function blocks() {
     rec.forEach(entity => {
         let title = entity[0];
+        let image = basePosterPath + size + entity[1];
+        if (!image)
+            image = '../img/no-poster.png';
+
         let card = document.createElement("div");
+        card.setAttribute('class', 'card');
+        card.style.backgroundImage = `url('${image}')`;
+
+        let cardOverlay = document.createElement('div');
+        cardOverlay.setAttribute('class', 'overlay');
+
         let h = document.createElement("h1");
-        card.appendChild(h);
         h.innerHTML = title;
+
+        card.appendChild(h);
+        card.appendChild(cardOverlay);
         wrapper.appendChild(card);
     });
 }
 
 var wrapper = document.getElementById('blocks');
+
